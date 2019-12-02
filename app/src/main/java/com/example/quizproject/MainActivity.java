15be9793543cfe06bcba24bad285a15d5cdb4056
yasir khan <ykhan5439@gmail.com>
2019-12-02 18:04:31 +0530

    added features

--- app/src/main/java/com/example/quizproject/MainActivity.java
+++ app/src/main/java/com/example/quizproject/MainActivity.java
@@ -3,12 +3,91 @@ package com.example.quizproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private QuizModel[] quizModels = new QuizModel[]{
+            new QuizModel(R.string.q10,true),
+            new QuizModel(R.string.q1,true),
+            new QuizModel(R.string.q2,false),
+            new QuizModel(R.string.q3,true),
+            new QuizModel(R.string.q4,false),
+            new QuizModel(R.string.q5,true),
+            new QuizModel(R.string.q6,false),
+            new QuizModel(R.string.q7,true),
+            new QuizModel(R.string.q8,true),
+            new QuizModel(R.string.q9,false),
+    };
+    final int incPro = (int) Math.ceil( 100.0/quizModels.length);
+
+    private Button btnTrue , btnFalse;
+    private TextView txtQuestion , txtQues;
+    private ProgressBar progressBar;
+    private int mUserValue;
+
+
+    private int mQuestionIndex;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        intiWidget();
+
+        QuizModel model = quizModels[mQuestionIndex];
+
+        txtQuestion.setText(model.getmQuestion());
+
+
+
+        btnTrue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userAnswerEvaluate(true);
+               changeButton();
+            }
+        });
+
+        btnFalse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userAnswerEvaluate(false);
+                changeButton();
+
+            }
+        });
+    }
+
+    public void changeButton(){
+        mQuestionIndex +=1;
+        mQuestionIndex = mQuestionIndex % 10;
+        QuizModel model = quizModels[mQuestionIndex];
+        txtQuestion.setText(model.getmQuestion());
+        progressBar.incrementProgressBy(incPro);
+        txtQues.setText(mUserValue + " / 10");
+    }
+
+    public void intiWidget(){
+        btnTrue = (Button)findViewById(R.id.btnTrue);
+        btnFalse = (Button) findViewById(R.id.btnFalse);
+        txtQuestion = (TextView) findViewById(R.id.txtQuestions);
+        txtQues = (TextView) findViewById(R.id.txtQues);
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+    }
+
+    public void userAnswerEvaluate(boolean guess){
+
+        if (quizModels[mQuestionIndex].ismAnswer() == guess){
+            Toast.makeText(getApplicationContext(),R.string.correct_text,Toast.LENGTH_SHORT).show();
+            mUserValue = mUserValue + 1;
+        }else {
+            Toast.makeText(getApplicationContext(),R.string.incorrect_text,Toast.LENGTH_SHORT).show();
+        }
     }
 }
